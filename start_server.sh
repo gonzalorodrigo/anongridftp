24369daf83b09c679893cc0ccf83c09463b6923f
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-11-15 13:31:35 -0700

    Start script does not overwrite PUBLIC_IP if set.

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,10 @@
 #
 # Requires that the HOSTNAME is configured
 
-export PUBLIC_IP=`dig +short $HOSTNAME`
+if [ -z ${PUBLIC_IP} ]; then
+  export PUBLIC_IP=`dig +short $HOSTNAME`
+  echo "PUBLIC_IP not set, setting IP to ${PUBLIC_IP}"
+fi
 
 docker-compose build
 docker-compose up $1 -d 
