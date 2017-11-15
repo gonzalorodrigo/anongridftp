f4676e7f6166e1703845369c660f55e18ef19353
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-11-15 12:26:20 -0700

    builds container

--- start_server.sh
+++ start_server.sh
@@ -3,9 +3,6 @@
 # Requires that the HOSTNAME is configured
 
 export PUBLIC_IP=`dig +short $HOSTNAME`
-# example on hot to reduce the number of ports
-export GLOBUS_HI_DATA_TCP_PORT=50100
-export GLOBUS_LOW_DATA_TCP_PORT=50000
-export GLOBUS_TCP_PORT_RANGE=50000,50100
 
-docker-compose up $1
+docker-compose build
+docker-compose up $1 -d 
