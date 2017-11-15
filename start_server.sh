a56d1e04731d0e0cace260d5f99e3f8ef9d9b3ac
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-11-15 10:10:29 -0700

    ADded default ports reduction

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,9 @@
 # Requires that the HOSTNAME is configured
 
 export PUBLIC_IP=`dig +short $HOSTNAME`
+# example on hot to reduce the number of ports
+export GLOBUS_HI_DATA_TCP_PORT=50100
+export GLOBUS_LOW_DATA_TCP_PORT=50000
+export GLOBUS_TCP_PORT_RANGE=50000,50100
 
 docker-compose up $1
